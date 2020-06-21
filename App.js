fb13b7541791842a710c7fca4622ebebc541ed6c
3douhao <3douhao@gmail.com>
2020-06-21 17:54:58 +0800

    lesson 4 completed

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage'
+
+const Hats = () => {
+  return (
+    <div>
+      <Link to='/'>HomePage</Link>
+      <h1>this is hats page</h1>
+    </div>
+  )
+}
 
 class App extends Component {
   render () {
-    return <HomePage />
+    return (
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={Hats} />
+      </Switch>
+    )
   }
 }
 
